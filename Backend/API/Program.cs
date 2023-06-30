83685f6e214b2e3eb7901747d7a20da9b42f3685
Jayakumar <jsinnapillai@gmail.com>
2023-06-30 11:35:29 +0100

    Service  Layer Implemented

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,16 +1,31 @@
 using API.GraphQL;
+using Core.Interfaces;
 using GraphQL.Server.Ui.Voyager;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var AllowSpecificOrgins = "_allowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(cors =>
+{
+    cors.AddPolicy(name: AllowSpecificOrgins, policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
 builder.Services.AddDbContextFactory<OMAContext>(options =>
 {
     options.UseInMemoryDatabase("InMemoryDB");
 });
+
+///
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 
 builder.Services.AddGraphQLServer()
 .AddQueryType<Query>()
